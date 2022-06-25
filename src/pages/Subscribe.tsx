3ea6cedce6e11a4e99d17ec698a92d1e4b1dc967
suicideDuck22 <rdorneles64@gmail.com>
2022-06-25 13:02:40 -0300

    Changed the way of use initial page img code-mockup on the subscribe screen

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import codeMockupImg from '../assets/code-mockup.png'
 
 export function Subscribe(){
     const navigate = useNavigate()
@@ -64,7 +65,7 @@ export function Subscribe(){
                 </div>
             </div>
 
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={codeMockupImg} className="mt-10" alt="" />
         </div>
     )
 }
